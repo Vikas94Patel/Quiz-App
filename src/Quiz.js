cf72beb7dd62dadd4977aeaf6c1c404ca1dc2849
Vikas Patel <vikspatel121@gmail.com>
2022-05-19 15:48:01 +0530

    Implement the Quiz

---
+++ src/Quiz.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import QuizData from "./QuizData";
+
+function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [attempted, setAttempt] = useState([]);
+  const [showScore, setShowScore] = useState(false);
+
+  const [score, setScore] = useState(0);
+
+  const handleOptionClick = (id, answer) => {
+    if (
+      answer === QuizData[currentQuestion].answer &&
+      !attempted.includes(id)
+    ) {
+      setScore(score + 1);
+      setAttempt((prev) => [...prev, id]);
+    } else if (
+      answer === QuizData[currentQuestion].answer &&
+      attempted.includes(id)
+    ) {
+    } else if (
+      answer !== QuizData[currentQuestion].answer &&
+      attempted.includes(id)
+    ) {
+      setScore(score - 1);
+    }
+  };
+
+  const handlePrevClick = () => {
+    setCurrentQuestion(currentQuestion - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentQuestion(currentQuestion + 1);
+
+    const nextQuestion = currentQuestion + 1;
+
+    if (nextQuestion < QuizData.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div>
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {QuizData.length}
+        </div>
+      ) : (
+        <div>
+          <div className="styles.question-section">
+            <div className="question-count">
+              <span>Question : {currentQuestion + 1}</span>
+            </div>
+            <div className="question-text">
+              <p>{QuizData[currentQuestion].question}</p>
+            </div>
+          </div>
+          <div className="answer-section">
+            {QuizData[currentQuestion].options.map((option) => (
+              <button
+                type="button"
+                onClick={() =>
+                  handleOptionClick(QuizData[currentQuestion].id, option)
+                }
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+          <div>
+            <button className="prevbutton" onClick={handlePrevClick}>
+              Previous
+            </button>
+            <button className="nextbutton" onClick={handleNextClick}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
---
+++ src/QuizData.js
@@ -0,0 +1,64 @@
+const QuizData = [
+  {
+    id: 1,
+    question: `What is the capital of India?`,
+    options: [`New Delhi`, `Punjab`, `Mumbai`, `Chennai`],
+    answer: `New Delhi`,
+  },
+  {
+    id: 2,
+    question: `What is the capital of Jammu And Kashmir?`,
+    options: [`New Delhi`, `Punjab`, `Mumbai`, `Shri Nagar`],
+    answer: `Shri Nagar`,
+  },
+  {
+    id: 3,
+    question: `What is the capital of Madhya Pradesh?`,
+    options: [`New Delhi`, `Bhopal`, `Mumbai`, `Chennai`],
+    answer: `Bhopal`,
+  },
+  {
+    id: 4,
+    question: `What is the capital of Gujarat?`,
+    options: [`New Delhi`, `Punjab`, `Gandhinagar`, `Chennai`],
+    answer: `Gandhinagar`,
+  },
+  {
+    id: 5,
+    question: `What is the capital of Rajasthan?`,
+    options: [`Jaipur`, `Punjab`, `Mumbai`, `Chennai`],
+    answer: `Jaipur`,
+  },
+  {
+    id: 6,
+    question: `What is the capital of Tamil Nadu?`,
+    options: [`Jaipur`, `Punjab`, `Mumbai`, `Chennai`],
+    answer: `Chennai`,
+  },
+  {
+    id: 7,
+    question: `What is the capital of Karnataka?`,
+    options: [`Jaipur`, `Punjab`, `Bangluru`, `Chennai`],
+    answer: `Bangluru`,
+  },
+  {
+    id: 8,
+    question: `What is the capital of Odissa?`,
+    options: [`Jaipur`, `Bhuvneshwar`, `Mumbai`, `Chennai`],
+    answer: `Bhuvneshwar`,
+  },
+  {
+    id: 9,
+    question: `What is the capital of Bihar?`,
+    options: [`Patna`, `Punjab`, `Mumbai`, `Chennai`],
+    answer: `Patna`,
+  },
+  {
+    id: 10,
+    question: `What is the capital of Uttar Pradesh?`,
+    options: [`Jaipur`, `Punjab`, `Mumbai`, `Lucknow`],
+    answer: `Lucknow`,
+  },
+];
+
+export default QuizData;
